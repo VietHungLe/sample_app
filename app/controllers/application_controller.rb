b443ccb5bc6c30d89dc2fe6f26ca40c8e3e26987
VietHungLe <viethungle1235@gmail.com>
2019-01-24 15:22:27 +0700

    Chapter4: Rails-flavored Ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
@@ -5,6 +6,15 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = if I18n.available_locales.include?(locale)
+                    locale
+                  else
+                    I18n.default_locale
+                  end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
